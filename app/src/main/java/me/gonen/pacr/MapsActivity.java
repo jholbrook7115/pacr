f644ce50948da13440d5eaf0848ed9c796ecb9c1
jholbrook7115 <holbrook002@gmail.com>
2015-09-05 07:47:17 -0400

    maps functioning with FusedLocationApi

--- app/src/main/java/me/gonen/pacr/MapsActivity.java
+++ app/src/main/java/me/gonen/pacr/MapsActivity.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,9 +23,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private LocationManager locationManager;
@@ -57,18 +57,19 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 //        locationListener = getLocationListener();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 3, getLocationListener());
-
+        currentLocation = new Location(LocationManager.NETWORK_PROVIDER);
         //currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+/*
 
-        if(currentLocation != null) {
+        if (currentLocation != null) {
             initializeMapElements();
-        }
-        else {
+        } else {
             Toast.makeText(getApplicationContext(), "Location Service is off!", Toast.LENGTH_LONG).show();
 
         }
+*/
     }
 
     @Override
@@ -129,17 +130,13 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         //Add marker
         myMarker = mMap.addMarker(new MarkerOptions().position(myLatLng).title("Marker"));
         //Add path line to map
-        addRouteWaypoint(new LatLng(myLatLng.latitude+1, myLatLng.longitude+1));
+        addRouteWaypoint(new LatLng(myLatLng.latitude + 1, myLatLng.longitude + 1));
 
         //Move camera to current location
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, zoomLevel));
         mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @Override
             public void onCameraChange(CameraPosition position) {
-                if (zoomLevel != position.zoom) {
-//                    isZooming = true;
-                }
-
                 zoomLevel = position.zoom;
             }
         });
@@ -147,9 +144,10 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     }
 
     public void addRouteWaypoint(LatLng newWaypoint) {
-        mMap.addPolyline(new PolylineOptions().
-                add(lastWaypoint, newWaypoint)
-                .width(10).color(Color.argb(75,0,0,255)));
+        if (lastWaypoint == null) lastWaypoint = newWaypoint;
+        mMap.addPolyline(new PolylineOptions()
+                .add(lastWaypoint, newWaypoint)
+                .width(10).color(Color.argb(75, 0, 0, 255)));
         lastWaypoint = newWaypoint;
     }
 
@@ -158,7 +156,10 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
             locationListener = new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
-                    currentLocation.set(location);
+
+                    if (currentLocation == null) currentLocation = new Location(location);
+                    else currentLocation.set(location);
+
                     double lat = 0;
                     double lon = 0;
                     if (location != null) {
@@ -169,9 +170,12 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
                     LatLng myLatLng = new LatLng(lat, lon);
 
                     //Add marker
-                    myMarker.setPosition(myLatLng);
+                    MarkerOptions defaultMarkerOption = new MarkerOptions().position(myLatLng);
+                    if (myMarker == null) myMarker = mMap.addMarker(defaultMarkerOption);
+                    else myMarker.setPosition(myLatLng);
 
-                    if(!routeDrawn) drawRoute();
+
+                    if (!routeDrawn) drawRoute();
 
                     //Move camera to current location
                     CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(myLatLng, zoomLevel);
@@ -197,46 +201,28 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         locationManager.requestSingleUpdate(locationManager.getBestProvider(new Criteria(), true), locationListener, Looper.myLooper());
         return locationListener;
     }
-    private void drawRoute(){
+
+    private void drawRoute() {
         //Add path line to map
         addRouteWaypoint(new LatLng(currentLocation.getLatitude() + 1, currentLocation.getLongitude()));
 
         routeDrawn = true;
     }
+
+
     @Override
-    public void onLocationChanged(Location location) {
-        currentLocation.set(location);
-        double lat = 0;
-        double lon = 0;
-        if (location != null) {
-            lat = location.getLatitude();
-            lon = location.getLongitude();
-        }
-
-        LatLng myLatLng = new LatLng(lat, lon);
-
-        //Add marker
-        mMap.addMarker(new MarkerOptions().position(myLatLng).title("Marker"));
-
-
-        //Move camera to current location
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(myLatLng, 10);
-        mMap.animateCamera(cameraUpdate);
-//        locationManager.removeUpdates(this);
+    public void onConnected(Bundle bundle) {
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        initializeMapElements();
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    public void onConnectionSuspended(int i) {
 
     }
 
     @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
