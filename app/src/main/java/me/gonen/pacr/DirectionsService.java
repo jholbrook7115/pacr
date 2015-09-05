cab840f2d641ae9aa12cb1322a019c5a3cf16c28
Gonen Shoham <gonensh@gmail.com>
2015-09-05 16:04:47 -0400

    Added build config files

--- app/src/main/java/me/gonen/pacr/DirectionsService.java
+++ app/src/main/java/me/gonen/pacr/DirectionsService.java
@@ -5,6 +5,7 @@ package me.gonen.pacr;
  */
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,170 +16,85 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.joda.time.DateTime;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.TravelMode;
+import com.google.maps.model.Unit;
 
 import android.util.Log;
 
 public class DirectionsService {
-    public final static String MODE_DRIVING = "driving";
-    public final static String MODE_WALKING = "walking";
+    DirectionsApi directionsApi;
 
-    public DirectionsService() { }
+    private GeoApiContext context;
 
-    public Document getDocument(LatLng start, LatLng end, String mode) {
-        String url = "http://maps.googleapis.com/maps/api/directions/xml?"
-                + "origin=" + start.latitude + "," + start.longitude
-                + "&destination=" + end.latitude + "," + end.longitude
-                + "&sensor=false&units=metric&mode="+mode;
-
-        try {
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpContext localContext = new BasicHttpContext();
-            HttpPost httpPost = new HttpPost(url);
-            HttpResponse response = httpClient.execute(httpPost, localContext);
-            InputStream in = response.getEntity().getContent();
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document doc = builder.parse(in);
-            return doc;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public DirectionsService(GeoApiContext context) {
+        this.context = context
+                .setQueryRateLimit(3)
+                .setConnectTimeout(1, TimeUnit.SECONDS)
+                .setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
     }
 
-    public String getDurationText (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("duration");
-        Node node1 = nl1.item(nl1.getLength() - 1);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "text"));
-        Log.i("DurationText", node2.getTextContent());
-        return node2.getTextContent();
+    public void testGetDirections() throws Exception {
+        DirectionsRoute[] routes = DirectionsApi.getDirections(context, "Sydney, AU",
+                "Melbourne, AU").await();
     }
 
-    public int getDurationValue (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("duration");
-        Node node1 = nl1.item(nl1.getLength() - 1);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "value"));
-        Log.i("DurationValue", node2.getTextContent());
-        return Integer.parseInt(node2.getTextContent());
+    public void testBuilder() throws Exception {
+        DirectionsRoute[] routes = DirectionsApi.newRequest(context)
+                .mode(TravelMode.BICYCLING)
+                .avoid(DirectionsApi.RouteRestriction.HIGHWAYS, DirectionsApi.RouteRestriction.TOLLS, DirectionsApi.RouteRestriction.FERRIES)
+                .units(Unit.METRIC)
+                .region("au")
+                .origin("Sydney")
+                .destination("Melbourne").await();
     }
 
-    public String getDistanceText (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("distance");
-        Node node1 = nl1.item(nl1.getLength() - 1);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "text"));
-        Log.i("DistanceText", node2.getTextContent());
-        return node2.getTextContent();
+    public void testTravelModeRoundTrip() throws Exception {
+        DirectionsRoute[] routes = DirectionsApi.newRequest(context)
+                .mode(TravelMode.BICYCLING)
+                .origin("Town Hall, Sydney")
+                .destination("Parramatta, NSW").await();
     }
 
-    public int getDistanceValue (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("distance");
-        Node node1 = nl1.item(nl1.getLength() - 1);
-        NodeList nl2 = node1.getChildNodes();
-        Node node2 = nl2.item(getNodeIndex(nl2, "value"));
-        Log.i("DistanceValue", node2.getTextContent());
-        return Integer.parseInt(node2.getTextContent());
+    public void testResponseTimesArePopulatedCorrectly() throws Exception {
+        DateTime now = new DateTime();
+        DirectionsRoute[] routes = DirectionsApi.newRequest(context)
+                .mode(TravelMode.TRANSIT)
+                .origin("Town Hall, Sydney")
+                .destination("Parramatta, NSW")
+                .departureTime(now)
+                .await();
+}
+
+    /**
+     * A simple query from Toronto to Montreal.
+     * {@url http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal}
+     */
+    public void testTorontoToMontreal() throws Exception {
+        DirectionsRoute[] routes = DirectionsApi.newRequest(context)
+                .origin("Toronto")
+                .destination("Montreal").await();
     }
 
-    public String getStartAddress (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("start_address");
-        Node node1 = nl1.item(0);
-        Log.i("StartAddress", node1.getTextContent());
-        return node1.getTextContent();
-    }
-
-    public String getEndAddress (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("end_address");
-        Node node1 = nl1.item(0);
-        Log.i("StartAddress", node1.getTextContent());
-        return node1.getTextContent();
-    }
-
-    public String getCopyRights (Document doc) {
-        NodeList nl1 = doc.getElementsByTagName("copyrights");
-        Node node1 = nl1.item(0);
-        Log.i("CopyRights", node1.getTextContent());
-        return node1.getTextContent();
-    }
-
-    public ArrayList<LatLng> getDirection (Document doc) {
-        NodeList nl1, nl2, nl3;
-        ArrayList<LatLng> listGeopoints = new ArrayList<LatLng>();
-        nl1 = doc.getElementsByTagName("step");
-        if (nl1.getLength() > 0) {
-            for (int i = 0; i < nl1.getLength(); i++) {
-                Node node1 = nl1.item(i);
-                nl2 = node1.getChildNodes();
-
-                Node locationNode = nl2.item(getNodeIndex(nl2, "start_location"));
-                nl3 = locationNode.getChildNodes();
-                Node latNode = nl3.item(getNodeIndex(nl3, "lat"));
-                double lat = Double.parseDouble(latNode.getTextContent());
-                Node lngNode = nl3.item(getNodeIndex(nl3, "lng"));
-                double lng = Double.parseDouble(lngNode.getTextContent());
-                listGeopoints.add(new LatLng(lat, lng));
-
-                locationNode = nl2.item(getNodeIndex(nl2, "polyline"));
-                nl3 = locationNode.getChildNodes();
-                latNode = nl3.item(getNodeIndex(nl3, "points"));
-                ArrayList<LatLng> arr = decodePoly(latNode.getTextContent());
-                for(int j = 0 ; j < arr.size() ; j++) {
-                    listGeopoints.add(new LatLng(arr.get(j).latitude, arr.get(j).longitude));
-                }
-
-                locationNode = nl2.item(getNodeIndex(nl2, "end_location"));
-                nl3 = locationNode.getChildNodes();
-                latNode = nl3.item(getNodeIndex(nl3, "lat"));
-                lat = Double.parseDouble(latNode.getTextContent());
-                lngNode = nl3.item(getNodeIndex(nl3, "lng"));
-                lng = Double.parseDouble(lngNode.getTextContent());
-                listGeopoints.add(new LatLng(lat, lng));
-            }
-        }
-
-        return listGeopoints;
-    }
-
-    private int getNodeIndex(NodeList nl, String nodename) {
-        for(int i = 0 ; i < nl.getLength() ; i++) {
-            if(nl.item(i).getNodeName().equals(nodename))
-                return i;
-        }
-        return -1;
-    }
-
-    private ArrayList<LatLng> decodePoly(String encoded) {
-        ArrayList<LatLng> poly = new ArrayList<LatLng>();
-        int index = 0, len = encoded.length();
-        int lat = 0, lng = 0;
-        while (index < len) {
-            int b, shift = 0, result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lat += dlat;
-            shift = 0;
-            result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lng += dlng;
-
-            LatLng position = new LatLng((double) lat / 1E5, (double) lng / 1E5);
-            poly.add(position);
-        }
-        return poly;
+    /**
+     * Going from Toronto to Montreal by bicycle, avoiding highways.
+     * {@url http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&avoid=highways&mode=bicycling}
+     */
+    public void testTorontoToMontrealByBicycleAvoidingHighways() throws Exception {
+        DirectionsRoute[] routes = DirectionsApi.newRequest(context)
+                .origin("Toronto")
+                .destination("Montreal")
+                .avoid(DirectionsApi.RouteRestriction.HIGHWAYS)
+                .mode(TravelMode.BICYCLING)
+                .await();
     }
 }
